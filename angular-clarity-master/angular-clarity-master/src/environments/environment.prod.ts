9804409c75e078b80be7c29a760a6193628a0df1
ashwinikaddekatc <ashwini.kad@dekatc.com>
2022-06-07 14:02:42 +0530

    regi error and ui

---
+++ angular-clarity-master/angular-clarity-master/src/environments/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  appName: 'My Application',
+  version: '11.2.13',
+  subVersion: '2021.05.13-01',
+  apiUrl: 'http://localhost:3000',
+  whiteUrl: 'http://localhost:3000',
+  blackUrl: 'http://localhost:3000/login',
+
+
+  backendurl: 'http://54.227.230.160:31167',
+  backendtoken: 'http://54.227.230.160:31167/token/',
+  backendport:'31167',
+  portalurl:'http://54.227.230.160:30165'
+};
---
+++ angular-clarity-master/angular-clarity-master/src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  appName: 'My Application',
+  version: '11.2.13',
+  subVersion: '2021.05.13-01',
+  apiUrl: 'http://localhost:3000',
+  whiteUrl: 'http://localhost:3000',
+  blackUrl: 'http://localhost:3000/login',
+
+  backendurl: 'http://localhost:9191',
+  backendtoken: 'http://localhost:9191/token/',
+  backendport:'9191',
+  portalurl:'http://54.227.230.160:30165'
+
+  // backendurl: 'http://54.227.230.160:31167',
+  // backendtoken: 'http://54.227.230.160:31167/token/',
+  // backendport:'31167',
+  // portalurl:'http://54.227.230.160:30165'
+};
